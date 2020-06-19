c9ba198da387519e6793c91bdb1675dcc12c1e59
demola07 <demolasobaki@gmail.com>
2020-06-19 21:01:34 +0100

    register user and encrypt password

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const USerSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please add a name'],
@@ -30,4 +31,10 @@ const USerSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model('User', USerSchema);
+// Encrypt password using bcryptjs
+UserSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model('User', UserSchema);
