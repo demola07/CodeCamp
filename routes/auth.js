8b8ff1b698c12dc09dad67447bf166db9b4cd0b6
demola07 <demolasobaki@gmail.com>
2020-06-27 01:14:26 +0100

    update user details

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,13 @@
 const express = require('express');
-const { register, login, getMe, forgotPassword, resetPassword } = require('../controllers/auth');
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  resetPassword,
+  updateDetails,
+  updatePassword,
+} = require('../controllers/auth');
 
 const { protect } = require('../middleware/auth');
 
@@ -10,6 +18,10 @@ router.post('/register', register);
 router.post('/login', login);
 
 router.get('/me', protect, getMe);
+
+router.put('/updatedetails', protect, updateDetails);
+
+router.put('/updatepassword', protect, updatePassword);
 
 router.post('/forgotpassword', forgotPassword);
 
