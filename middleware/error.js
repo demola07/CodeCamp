d2ea6c5b9abb42b2e769ced63f9c6f52e5a7070d
demola07 <demolasobaki@gmail.com>
2020-07-01 21:41:45 +0100

    security: prevent nosql injection and sanitize data

--- middleware/error.js
+++ middleware/error.js
@@ -1,34 +1,34 @@
-const ErrorResponse = require('../utils/errorResponse');
+const ErrorResponse = require('../utils/errorResponse')
 
 const errorHandler = (err, req, res, next) => {
-  let error = { ...err };
-  error.message = err.message;
+  let error = { ...err }
+  error.message = err.message
 
   // Log to console for dev
-  console.log(err.stack.red);
+  console.log(err.stack.red)
 
   //   Mongoose bad ObjectId
   if (err.name === 'CastError') {
-    const message = `Resource not found with id of ${err.value}`;
-    error = new ErrorResponse(message, 404);
+    const message = `Resource not found`
+    error = new ErrorResponse(message, 404)
   }
 
   //   Mongoose duplicate key
   if (err.code === 11000) {
-    const message = `Dulpicate field value entered`;
-    error = new ErrorResponse(message, 400);
+    const message = `Dulpicate field value entered`
+    error = new ErrorResponse(message, 400)
   }
 
   //Mongoose Validation Errors
   if (err.name === 'ValidationError') {
-    const message = Object.values(err.errors).map((val) => val.message);
-    error = new ErrorResponse(message, 400);
+    const message = Object.values(err.errors).map((val) => val.message)
+    error = new ErrorResponse(message, 400)
   }
 
   res.status(error.statusCode || 500).json({
     success: false,
-    error: error.message || 'Server Error',
-  });
-};
+    error: error.message || 'Server Error'
+  })
+}
 
-module.exports = errorHandler;
+module.exports = errorHandler
--- server.js
+++ server.js
@@ -1,68 +1,72 @@
-const path = require('path');
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const colors = require('colors');
-const fileupload = require('express-fileupload');
-const cookieParser = require('cookie-parser');
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const colors = require('colors')
+const fileupload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
+const mongoSanitize = require('express-mongo-sanitize')
 
-const connectDB = require('./config/db');
-const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db')
+const errorHandler = require('./middleware/error')
 
 //load env files
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: './config/config.env' })
 
 //Connect to Database
-connectDB();
+connectDB()
 
 //Route files
-const bootcamps = require('./routes/bootcamps');
-const courses = require('./routes/courses');
-const auth = require('./routes/auth');
-const users = require('./routes/users');
-const reviews = require('./routes/reviews');
+const bootcamps = require('./routes/bootcamps')
+const courses = require('./routes/courses')
+const auth = require('./routes/auth')
+const users = require('./routes/users')
+const reviews = require('./routes/reviews')
 
-const app = express();
+const app = express()
 
 // Body Parser
-app.use(express.json());
+app.use(express.json())
 
 // Cookie parser
-app.use(cookieParser());
+app.use(cookieParser())
 
 //Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+  app.use(morgan('dev'))
 }
 
 // File uploading
-app.use(fileupload());
+app.use(fileupload())
+
+// Sanitize Data
+app.use(mongoSanitize())
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Mount routers
-app.use('/api/v1/bootcamps', bootcamps);
-app.use('/api/v1/courses', courses);
-app.use('/api/v1/auth', auth);
-app.use('/api/v1/users', users);
-app.use('/api/v1/reviews', reviews);
+app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/courses', courses)
+app.use('/api/v1/auth', auth)
+app.use('/api/v1/users', users)
+app.use('/api/v1/reviews', reviews)
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 const server = app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on PORT:${process.env.PORT}`.yellow.bold
   )
-);
+)
 
 // Handle Unhandled Promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
+  console.log(`Error: ${err.message}`.red)
 
   //Close server & exit process
-  server.close(() => process.exit(1));
-});
+  server.close(() => process.exit(1))
+})
