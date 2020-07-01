93028a67e6206873305a82d2593a03ab875bc8b1
demola07 <demolasobaki@gmail.com>
2020-07-01 21:30:16 +0100

    implement user logout

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,44 +1,48 @@
-const jwt = require('jsonwebtoken');
-const asyncHandler = require('./async');
-const ErrorResponse = require('../utils/errorResponse');
-const User = require('../models/User');
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('./async')
+const ErrorResponse = require('../utils/errorResponse')
+const User = require('../models/User')
 
 // Protect Routes
 exports.protect = asyncHandler(async (req, res, next) => {
-  let token;
+  let token
 
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    token = req.headers.authorization.split(' ')[1];
+    // Set token from Bearer token in header
+    token = req.headers.authorization.split(' ')[1]
   }
-  // else if(req.cookies.token){
-  //     token = req.cookies.token
+  // else if (req.cookies.token) {
+  //   // Set token from cookie
+  //   token = req.cookies.token
   // }
 
   // Make sure token exists
   if (!token) {
-    return next(new ErrorResponse('Not Authorized to access this route', 401));
+    return next(new ErrorResponse('Not Authorized to access this route', 401))
   }
 
   try {
     // Verify token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decoded);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-    req.user = await User.findById(decoded.id);
-    next();
+    req.user = await User.findById(decoded.id)
+    next()
   } catch (error) {
-    return next(new ErrorResponse('Not Authorized to access this route', 401));
+    return next(new ErrorResponse('Not Authorized to access this route', 401))
   }
-});
+})
 
 // Grant access to specific roles
 exports.authorize = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       return next(
-        new ErrorResponse(`User role '${req.user.role}' is unauthorized to access this route`, 403)
-      );
+        new ErrorResponse(
+          `User role '${req.user.role}' is not authorized to access this route`,
+          403
+        )
+      )
     }
-    next();
-  };
-};
+    next()
+  }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express')
 const {
   register,
   login,
@@ -7,24 +7,27 @@ const {
   resetPassword,
   updateDetails,
   updatePassword,
-} = require('../controllers/auth');
+  logout
+} = require('../controllers/auth')
 
-const { protect } = require('../middleware/auth');
+const { protect } = require('../middleware/auth')
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/register', register);
+router.post('/register', register)
 
-router.post('/login', login);
+router.post('/login', login)
 
-router.get('/me', protect, getMe);
+router.get('/logout', logout)
 
-router.put('/updatedetails', protect, updateDetails);
+router.get('/me', protect, getMe)
 
-router.put('/updatepassword', protect, updatePassword);
+router.put('/updatedetails', protect, updateDetails)
 
-router.post('/forgotpassword', forgotPassword);
+router.put('/updatepassword', protect, updatePassword)
 
-router.put('/resetpassword/:resettoken', resetPassword);
+router.post('/forgotpassword', forgotPassword)
 
-module.exports = router;
+router.put('/resetpassword/:resettoken', resetPassword)
+
+module.exports = router
