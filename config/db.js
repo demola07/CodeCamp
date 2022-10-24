bc37d916ae43959dfd61a792991678413f4df51c
Ademola Sobaki <demolasobaki@gmail.com>
2022-10-24 12:10:13 +0100

    update mongoose config,a nd html file

--- config/db.js
+++ config/db.js
@@ -1,14 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  });
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+		})
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
-};
+		console.log(
+			`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+		)
+	} catch (error) {
+		console.error(`Error: ${error.message}`.red.underline.bold)
+		process.exit(1)
+	}
+	// const conn = await mongoose.connect(process.env.MONGO_URI, {
+	//   useNewUrlParser: true,
+	//   useCreateIndex: true,
+	//   useFindAndModify: false,
+	//   useUnifiedTopology: true,
+	// });
+}
 
-module.exports = connectDB;
+module.exports = connectDB
+
+// import mongoose from 'mongoose'
+
+// const connectDB = async () => {
+// 	try {
+// 		const conn = await mongoose.connect(process.env.MONGO_URI)
+
+// 		console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+// 	} catch (error) {
+// 		console.error(`Error: ${error.message}`.red.underline.bold)
+// 		process.exit(1)
+// 	}
+// }
+
+// export default connectDB
